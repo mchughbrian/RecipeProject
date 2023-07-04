0fdb5a196bef5009a54a5d0f1f1875babcee9a0e
brianmchugh <brianmchugh02@gmail.com>
2023-07-04 14:57:35 -0400

    Added Recipe Class to models.py

--- RecipeArchive/models.py
+++ RecipeArchive/models.py
@@ -1,3 +1,46 @@
-from django.db import models
-
 # Create your models here.
+# This is a command-line utility that lets you interact with your project.
+
+# Django's built-in models and User model are imported
+from django.db import models
+from django.contrib.auth.models import User
+
+# Recipe model is defined
+class Recipe(models.Model):
+    # name of the recipe as a string of maximum length 200 characters
+    name = models.CharField(max_length=200)
+
+    # description of the recipe as a text field
+    description = models.TextField()
+
+    # image of the recipe, uploaded to the "recipes/" directory
+    image = models.ImageField(upload_to='recipes/')
+
+    # cuisine of the recipe as a string of maximum length 100 characters
+    cuisine = models.CharField(max_length=100)
+
+    MEAL_TYPE_CHOICES = [
+        ('Breakfast', 'Breakfast'),
+        ('Lunch', 'Lunch'),
+        ('Dinner', 'Dinner'),
+        ('Dessert', 'Dessert'),
+        ('Snack', 'Snack'),
+        ('Drink', 'Drink'),
+        ('Other', 'Other'),
+    ]
+
+    # other fields...
+
+    meal_type = models.CharField(max_length=100, choices=MEAL_TYPE_CHOICES, default='Other')
+
+    # protein type of the recipe as a string of maximum length 100 characters
+    protein_type = models.CharField(max_length=100)
+
+    # rating of the recipe as an integer
+    rating = models.IntegerField()
+
+    # date when the recipe was added, automatically set to the current date and time
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    # the user who added the recipe, linked via a foreign key to the User model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
