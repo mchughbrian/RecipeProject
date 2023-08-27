35184dca6d74299a91cc70773a52b16063a9fad9
brianmchugh <brianmchugh02@gmail.com>
2023-08-26 20:25:34 -0400

    Updating to remove private key. Moved to local .env file

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*ij0(zsxxfp!5gg!_n=zpxi5d$to$dhp4bgdbdtq*@ekwmkqnj'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
