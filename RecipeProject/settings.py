13371e25548cc00a96c0228ea30e8dabe1bbcc0a
brianmchugh <brianmchugh02@gmail.com>
2023-08-26 20:30:11 -0400

    Fixed bug importing secret key

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -22,6 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
