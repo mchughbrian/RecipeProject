d0ac7fee4dd34dadc110324bd2545e723e3404f1
brianmchugh <brianmchugh02@gmail.com>
2023-12-03 17:01:12 -0500

    Clean up URLs

--- RecipeProject/urls.py
+++ RecipeProject/urls.py
@@ -15,13 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-#urlpatterns = [
-#   path('admin/', admin.site.urls),
-#]
-
-# Django's built-in path function and our views are imported
 from django.contrib.auth.views import LoginView
 from RecipeArchive import views
 from django.contrib.auth import views as auth_views
