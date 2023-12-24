397692882dfe1dbc4aa6aeac4f9a0db6161b106c
brianmchugh <brianmchugh02@gmail.com>
2023-12-24 13:30:33 -0500

    Making registration form look like log-in form

--- RecipeArchive/forms.py
+++ RecipeArchive/forms.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    cookbook_name = forms.CharField(max_length=100, required=True, help_text='Name of your cookbook')
+    cookbook_name = forms.CharField(max_length=100, required=True)
 
     class Meta:
         model = User
