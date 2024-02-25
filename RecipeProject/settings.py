441b1bcf2a159633c2149c3893359cd8e806fb95
brianmchugh <brianmchugh02@gmail.com>
2024-02-25 15:42:20 -0500

    Updates for deployment

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -29,11 +29,11 @@ dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 SECRET_KEY = os.environ['SECRET_KEY']
-#API_KEY = os.environ['API_KEY']
-#API_HOST = os.environ['API_HOST']
+API_KEY = os.environ['API_KEY']
+API_HOST = os.environ['API_HOST']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
