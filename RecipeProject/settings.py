d5a342babd7299a5eedc43a1ad3c18ceb6bf29d7
brianmchugh <brianmchugh02@gmail.com>
2024-02-25 22:27:12 -0500

    Updates for deployment

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -157,6 +157,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
