ca5b0d9cf6be72bef0b1faee08fe5c4991cbc20a
brianmchugh <brianmchugh02@gmail.com>
2024-02-26 21:28:13 -0500

    Updates for deployment

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -155,7 +155,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
