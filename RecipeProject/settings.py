fbd97e3e257d7e407ea4019634efaa52995fe3cb
brianmchugh <brianmchugh02@gmail.com>
2024-02-26 21:58:59 -0500

    Updates for deployment

--- RecipeProject/settings.py
+++ RecipeProject/settings.py
@@ -35,8 +35,7 @@ API_HOST = os.environ['API_HOST']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-#ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 # Application definition
 
