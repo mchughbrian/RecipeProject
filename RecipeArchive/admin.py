3ac9e54626dd54e53c536a980bd0af2b658521ab
brianmchugh <brianmchugh02@gmail.com>
2024-03-03 15:49:38 -0500

    Updating Admin View

--- RecipeArchive/admin.py
+++ RecipeArchive/admin.py
@@ -5,3 +5,11 @@ from .models import Recipe, Profile
 
 admin.site.register(Recipe)
 admin.site.register(Profile)
+
+class RecipeAdmin(admin.ModelAdmin):
+    ordering = ('user__id',)
+    list_display = ('name', 'user_id_display',)
+
+    def user_id_display(self, obj):
+        return obj.user.id
+    user_id_display.short_description = 'User ID'
