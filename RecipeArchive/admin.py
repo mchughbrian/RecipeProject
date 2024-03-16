5b914981806b9d82fc33860b3552a036c3395e6f
brianmchugh <brianmchugh02@gmail.com>
2024-03-15 21:19:25 -0400

    Attempting to fix admin view for recipes

--- RecipeArchive/admin.py
+++ RecipeArchive/admin.py
@@ -1,15 +1,20 @@
 from django.contrib import admin
 from .models import Recipe, Profile
 
-# Register your models here.
-
-admin.site.register(Recipe)
-admin.site.register(Profile)
 
 class RecipeAdmin(admin.ModelAdmin):
-    ordering = ('user__id',)
-    list_display = ('name', 'user_id_display',)
+    ordering = ('user__id',)  # This orders the recipes in the admin list view by user ID.
+    list_display = ('name', 'user_id_display',)  # This specifies which columns to display.
 
     def user_id_display(self, obj):
         return obj.user.id
-    user_id_display.short_description = 'User ID'
+    user_id_display.short_description = 'User ID'  # This sets a custom column name.
+
+    # If you want to make the User ID column sortable, you can add:
+    user_id_display.admin_order_field = 'user__id'
+
+
+# register model and the custom admin class
+admin.site.register(RecipeAdmin)
+admin.site.register(Recipe)
+admin.site.register(Profile)
