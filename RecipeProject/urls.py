b8362590815060e3e5df11b24a785520413cf7c2
brianmchugh <brianmchugh02@gmail.com>
2024-04-28 12:53:58 -0400

    update media url when in development

--- RecipeProject/urls.py
+++ RecipeProject/urls.py
@@ -72,3 +72,5 @@ urlpatterns = [
 
 ] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
